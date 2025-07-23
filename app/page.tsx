965b68ea9bb5adb658318b6d784ead108eaa32c8
Ttoto Phandolack <totophandolack915@gmail.com>
2025-07-23 11:05:58 +0700

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import FeaturedToursSection from '@/components/FeaturedToursSection';
 import ExperienceSection from '@/components/ExperienceSection';
 import GallerySection from '@/components/GallerySection';
 import TestimonialsSection from '@/components/TestimonialsSection';
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 
@@ -19,6 +20,7 @@ const Home = () => {
         <GallerySection/>
         <TestimonialsSection/>
       </main>
+      <Footer />
     </div>
   );
 };
