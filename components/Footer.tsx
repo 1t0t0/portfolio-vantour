c5b661fbc253d6089462ef87eb7172aae5b69f1c
Ttoto Phandolack <totophandolack915@gmail.com>
2025-07-28 10:54:22 +0700

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,11 +26,7 @@ const Footer = () => {
                 <h3 className="text-2xl font-bold text-gray-800 font-prompt">
                   <span className="text-orange-400">LAMBO</span> CAR RENTAL
                 </h3>
-                <div className="text-orange-400">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/>
-                  </svg>
-                </div>
+                
               </div>
               <p className="text-sm text-gray-500 italic font-sarabun">
                 ถึงเวลาเดินทางท่องโลกแล้ว
