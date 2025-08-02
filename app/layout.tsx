74d4695c1bac30ac0b4fbd50483701f569df0295
Ttoto Phandolack <totophandolack915@gmail.com>
2025-08-02 16:48:15 +0700

    add icon to tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,11 @@ export default function RootLayout({
   return (
     <html lang="th">
       <head>
+        {/* Favicon */}
+        <link rel="icon" type="image/jpeg" href="https://res.cloudinary.com/dmt2zysfc/image/upload/v1754128054/logo_uzqrpw_orchpb.png" />
+        <link rel="shortcut icon" type="image/jpeg" href="https://res.cloudinary.com/dmt2zysfc/image/upload/v1754128054/logo_uzqrpw_orchpb.png" />
+        <link rel="apple-touch-icon" href="https://res.cloudinary.com/dmt2zysfc/image/upload/v1754128054/logo_uzqrpw_orchpb.png" />
+        
         {/* Google Fonts สำหรับภาษาไทย */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
